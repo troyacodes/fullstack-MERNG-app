d039d14251f2031385e734bef41866b525c85f8d
Michael <q@mymac.local>
2019-11-01 16:14:23 -0400

    set up create and delete post mutations

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,5 +4,9 @@ const usersResolvers = require('./users');
 module.exports = {
   Query: {
     ...postsResolvers.Query
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation
   }
 };
