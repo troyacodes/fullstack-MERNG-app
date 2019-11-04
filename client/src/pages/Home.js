2f874c16ef13b0fa3c9eb2e3872a5d82d031eff4
Michael <q@mymac.local>
2019-11-04 16:27:17 -0500

    add comment count and like count to resolver

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 function Home() {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+  if (data) {
+    console.log(data);
+  }
   return (
     <div>
       <h1>Home Page</h1>
     </div>
   );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likes {
+        username
+      }
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 export default Home;
